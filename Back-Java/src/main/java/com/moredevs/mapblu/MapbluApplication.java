22b318275b0526f8cdb60ad08e26fe4e216b8870
jeanjacintho <jeanjacintho@yahoo.com.br>
2025-12-02 12:48:08 -0300

    feat: criado models e enums de ocorrencia e problema

---
+++ Back-Java/src/main/java/com/moredevs/mapblu/MapbluApplication.java
@@ -0,0 +1,13 @@
+package com.moredevs.mapblu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapbluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapbluApplication.class, args);
+	}
+
+}
